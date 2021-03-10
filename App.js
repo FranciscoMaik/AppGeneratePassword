b8d7ef86243e3cb19bfc6ee190cdc9cb660c8a1c
FranciscoMaik <chiconunes3081@gmail.com>
2021-03-10 16:40:10 +0000

    feat(all): application initialization
    Created interface and functions

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
+import Slider from '@react-native-community/slider';
+import Clipboard from 'expo-clipboard';
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+export default function App(){
+  const [password, setPassword] = useState('');
+  const [size, setSize] = useState(5);
+  
+  function generatePass(){
+    let pass = "";
+    for(let i = 0, n = charset.length; i < size; i++){
+      pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    setPassword(pass);
+  }
+
+  function copyPass(){
+    Clipboard.setString(password);
+    alert('Senha copiada com sucesso!');
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={require('./src/assets/logo.png')}
+        style={styles.logo}
+      />
+
+      <Text style={styles.title}>{ size } Caracteres</Text>
+      <View style={styles.area}>
+
+        <Slider 
+          style={{height: 50}}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor ="#ff0000"
+          maximumTrackTintColor = "#000000"
+          value={size}
+          onValueChange={(value) => setSize(value.toFixed(0))}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={generatePass}>
+        <Text style={styles.buttonText}>Gerar senha</Text>
+      </TouchableOpacity>
+
+      {password !== "" && (
+        <View style={styles.area}>
+          <Text style={styles.password} onLongPress={copyPass}> { password } </Text>
+        </View>
+      )}
+      
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f3f3ff",
   },
-});
+  logo:{
+    marginBottom: 60,
+  },
+  title:{
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  area:{
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: "#ffffff",
+    width: "90%",
+    borderRadius: 7,
+  },
+  button:{
+    backgroundColor: "#ffa200",
+    width: "80%",
+    height: 50,
+    borderRadius: 7,
+    marginBottom: 25,
+    
+    justifyContent: "center",
+    alignItems: "center",
+
+  },
+  buttonText:{
+    fontSize: 20,
+    color: "#ffffff",
+    fontWeight: "bold",
+  },
+  password:{
+    padding: 10,
+    textAlign:"center",
+    fontSize: 20,
+  }
+})
